dbdc46dab2be9870c039abfbc5133f9eec1da15e
rfikes4 <robertfikesiv@RobertFikesIV.local>
2017-09-03 15:03:51 -0600

    initial

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,24 +3,40 @@
 		<div id="content"></div>
 		<div id="midi-container">
 			<div id="home-bg">
-				<div id="p1" class="midi-pattern" ref="p1"></div>
-				<div id="p2" class="midi-pattern" ref="p2"></div>
-				<div id="p3" class="midi-pattern" ref="p3"></div>
-				<div id="p4" class="midi-pattern" ref="p4"></div>
-				<div id="p5" class="midi-pattern" ref="p5"></div>
-				<div id="p6" class="midi-pattern" ref="p6"></div>
-				<div id="p7" class="midi-pattern" ref="p7"></div>
-				<div id="p8" class="midi-pattern" ref="p8"></div>
+				<div class="p-container" ref="p1">
+					<div id="p1" class="midi-pattern"></div>
+				</div>
+				<div class="p-container" ref="p2">
+					<div id="p2" class="midi-pattern"></div>
+				</div>
+				<div class="p-container" ref="p3">
+					<div id="p3" class="midi-pattern"></div>
+				</div>
+				<div class="p-container" ref="p4">
+					<div id="p4" class="midi-pattern"></div>
+				</div>
+				<div class="p-container" ref="p5">
+					<div id="p5" class="midi-pattern"></div>
+				</div>
+				<div class="p-container" ref="p6">
+					<div id="p6" class="midi-pattern"></div>
+				</div>
+				<div class="p-container" ref="p7">
+					<div id="p7" class="midi-pattern"></div>
+				</div>
+				<div class="p-container" ref="p8">
+					<div id="p8" class="midi-pattern"></div>
+				</div>
 			</div>
-			<div id="midi">
+			<div id="midi" ref="m">
 				<div id="l3" class="petal" ref="l3"></div>
 				<div id="l2" class="petal" ref="l2"></div>
 				<div id="l1" class="petal" ref="l1"></div>				
 				<div id="r3" class="petal" ref="r3"></div>
 				<div id="r2" class="petal" ref="r2"></div>
 				<div id="r1" class="petal" ref="r1"></div>
-				<div id="midi-bg">
-					<div id="midi-bg-inner">
+				<div id="midi-bg" ref="m1">
+					<div id="midi-bg-inner" ref="m2">
 						<div id="e1" class="eq" ref="e1"></div>
 						<div id="e2" class="eq" ref="e2"></div>
 						<div id="e3" class="eq" ref="e3"></div>
@@ -38,31 +54,31 @@
 						<div id="e7a" class="eq" ref="e7a"></div>
 						<div id="e8a" class="eq" ref="e8a"></div>
 						<div id="midi-btns">
-							<div id="horn1" class="midi-btn q-horn vQ" @click="toggle"></div>
-							<div id="vocal2" class="midi-btn q-vocal vQ" @click="toggle"></div>
-							<div id="ensb2" class="midi-btn q-ensb" @click="toggle"></div>
-							<div id="drum5" class="midi-btn q-drum2" @click="toggle"></div>
-							<div id="drum6" class="midi-btn q-drum2" @click="toggle"></div>
-							<div id="vocal1" class="midi-btn q-voca vQ" @click="toggle"></div>
-							<div id="horn2" class="midi-btn q-horn vQ" @click="toggle"></div>
-							<div id="hi2" class="midi-btn q-hi" @click="toggle"></div>
-							<div id="drum8" class="midi-btn q-drum2" @click="toggle"></div>
-							<div id="drum7" class="midi-btn q-drum2" @click="toggle"></div>
-							<div id="ensb1" class="midi-btn q-ensb" @click="toggle"></div>
-							<div id="hi1" class="midi-btn q-hi" @click="toggle"></div>
-							<div id="hi3" class="midi-btn q-hi" @click="toggle"></div>
-							<div id="hi5" class="midi-btn q-hi" @click="toggle"></div>
-							<div id="ens3" class="midi-btn q-ens" @click="toggle"></div>
-							<div id="drum1" class="midi-btn q-drum1" @click="toggle"></div>
-							<div id="drum2" class="midi-btn q-drum1" @click="toggle"></div>
-							<div id="hi4" class="midi-btn q-hi" @click="toggle"></div>
-							<div id="ens2" class="midi-btn q-ens" @click="toggle"></div>
-							<div id="ens5" class="midi-btn q-ens" @click="toggle"></div>
-							<div id="drum4" class="midi-btn q-drum1" @click="toggle"></div>
-							<div id="drum3" class="midi-btn q-drum1" @click="toggle"></div>
-							<div id="ens1" class="midi-btn q-ens" @click="toggle"></div>
-							<div id="ens4" class="midi-btn q-ens" @click="toggle"></div>
-							<div id="ens6" class="midi-btn q-ens" @click="toggle"></div>
+							<div id="horn1" class="midi-btn q-horn vQ" ref="b1" @click="toggle"></div>
+							<div id="vocal2" class="midi-btn q-vocal vQ" ref="b2" @click="toggle"></div>
+							<div id="ensb2" class="midi-btn q-ensb" ref="b3" @click="toggle"></div>
+							<div id="drum5" class="midi-btn q-drum2" ref="b4" @click="toggle"></div>
+							<div id="drum6" class="midi-btn q-drum2" ref="b5" @click="toggle"></div>
+							<div id="vocal1" class="midi-btn q-voca vQ" ref="b6" @click="toggle"></div>
+							<div id="horn2" class="midi-btn q-horn vQ" ref="b7" @click="toggle"></div>
+							<div id="hi2" class="midi-btn q-hi" ref="b8" @click="toggle"></div>
+							<div id="drum8" class="midi-btn q-drum2" ref="b9" @click="toggle"></div>
+							<div id="drum7" class="midi-btn q-drum2" ref="b10" @click="toggle"></div>
+							<div id="ensb1" class="midi-btn q-ensb" ref="b11" @click="toggle"></div>
+							<div id="hi1" class="midi-btn q-hi" ref="b12" @click="toggle"></div>
+							<div id="hi3" class="midi-btn q-hi" ref="b13" @click="toggle"></div>
+							<div id="hi5" class="midi-btn q-hi" ref="b14" @click="toggle"></div>
+							<div id="ens3" class="midi-btn q-ens" ref="b15" @click="toggle"></div>
+							<div id="drum1" class="midi-btn q-drum1" ref="b16" @click="toggle"></div>
+							<div id="drum2" class="midi-btn q-drum1" ref="b17" @click="toggle"></div>
+							<div id="hi4" class="midi-btn q-hi" ref="b18" @click="toggle"></div>
+							<div id="ens2" class="midi-btn q-ens" ref="b19" @click="toggle"></div>
+							<div id="ens5" class="midi-btn q-ens" ref="b20" @click="toggle"></div>
+							<div id="drum4" class="midi-btn q-drum1" ref="b21" @click="toggle"></div>
+							<div id="drum3" class="midi-btn q-drum1" ref="b22" @click="toggle"></div>
+							<div id="ens1" class="midi-btn q-ens" ref="b23" @click="toggle"></div>
+							<div id="ens4" class="midi-btn q-ens" ref="b24" @click="toggle"></div>
+							<div id="ens6" class="midi-btn q-ens" ref="b25" @click="toggle"></div>
 						</div>
 					</div>
 				</div>
@@ -89,6 +105,34 @@
 				r1: null,
 				r2: null,
 				r3: null,
+				m: null,
+				m1: null,
+				m2: null,
+				b1: null,
+				b2: null,
+				b3: null,
+				b4: null,
+				b5: null,
+				b6: null,
+				b7: null,
+				b8: null,
+				b9: null,
+				b10: null,
+				b11: null,
+				b12: null,
+				b13: null,
+				b14: null,
+				b15: null,
+				b16: null,
+				b17: null,
+				b18: null,
+				b19: null,
+				b20: null,
+				b21: null,
+				b22: null,
+				b23: null,
+				b24: null,
+				b25: null,
 				e1: null,
 				e2: null,
 				e3: null,
@@ -132,6 +176,36 @@
 			this.r1 = this.$refs.r1;
 			this.r2 = this.$refs.r2;
 			this.r3 = this.$refs.r3;
+
+			this.m = this.$refs.m;
+			this.m1 = this.$refs.m1;
+			this.m2 = this.$refs.m2;
+
+			this.b1 = this.$refs.b1;
+			this.b2 = this.$refs.b2;
+			this.b3 = this.$refs.b3;
+			this.b4 = this.$refs.b4;
+			this.b5 = this.$refs.b5;
+			this.b6 = this.$refs.b6;
+			this.b7 = this.$refs.b7;
+			this.b8 = this.$refs.b8;
+			this.b9 = this.$refs.b9;
+			this.b10 = this.$refs.b10;
+			this.b11 = this.$refs.b11;
+			this.b12 = this.$refs.b12;
+			this.b13 = this.$refs.b13;
+			this.b14 = this.$refs.b14;
+			this.b15 = this.$refs.b15;
+			this.b16 = this.$refs.b16;
+			this.b17 = this.$refs.b17;
+			this.b18 = this.$refs.b18;
+			this.b19 = this.$refs.b19;
+			this.b20 = this.$refs.b20;
+			this.b21 = this.$refs.b21;
+			this.b22 = this.$refs.b22;
+			this.b23 = this.$refs.b23;
+			this.b24 = this.$refs.b24;
+			this.b25 = this.$refs.b25
 
 			this.e1 = this.$refs.e1;
 			this.e2 = this.$refs.e2;
@@ -181,46 +255,93 @@
 				ens6: new Howl({src: ['static/audio/loops/ens6.mp3']})
 			}
 
-			// var leaf = new TimelineMax({repeat: -1, yoyo: true});
-			TweenMax.to(this.l1, 4.8, {rotation: 7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 0});
-			TweenMax.to(this.l2, 4.8, {rotation: 7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 1.2});
-			TweenMax.to(this.l3, 4.8, {rotation: 7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 2.4});
-			TweenMax.to(this.r1, 4.8, {rotation: -7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 0.6});
-			TweenMax.to(this.r2, 4.8, {rotation: -7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 1.8});
-			TweenMax.to(this.r3, 4.8, {rotation: -7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 3.0});
-
-			var bg = new TimelineMax();
-			// bg.to(this.p1, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "0")
-			// bg.to(this.p2, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "8")
-			// bg.to(this.p3, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "16")
-			// bg.to(this.p4, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "24")
-			// bg.to(this.p5, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "32")
-			// bg.to(this.p6, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "40")
-			// bg.to(this.p7, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "48")
-			// bg.to(this.p8, 16, {width: 4000, height: 4000, ease: Power1.easeIn}, "56")
-
-			// bg.to(this.p1, 19.2, {scale: 6, ease: Power4.easeOut}, "0")
-			// bg.to(this.p1, 0, {scale: 1}, "9.6")
-			// bg.to(this.p2, 19.2, {scale: 6, ease: Power4.easeOut}, "2.4")
-			// bg.to(this.p2, 0, {scale: 1}, "9.6")
-			// bg.to(this.p3, 19.2, {scale: 6, ease: Power4.easeOut}, "4.8")
-			// bg.to(this.p3, 0, {scale: 1}, "12")
-			// bg.to(this.p4, 19.2, {scale: 6, ease: Power4.easeOut}, "7.2")
-			// bg.to(this.p4, 0, {scale: 1}, "14.4")
-			// bg.to(this.p5, 19.2, {scale: 6, ease: Power4.easeOut}, "9.6")
-			// bg.to(this.p5, 0, {scale: 1}, "16.8")
-			// bg.to(this.p6, 19.2, {scale: 6, ease: Power4.easeOut}, "12")
-			// bg.to(this.p6, 0, {scale: 1}, "19.2")
-			// bg.to(this.p7, 19.2, {scale: 6, ease: Power4.easeOut}, "14.4")
-			// bg.to(this.p7, 0, {scale: 1}, "21.6")
-			// bg.to(this.p8, 19.2, {scale: 6, ease: Power4.easeOut}, "16.8")
-			// bg.to(this.p8, 0, {scale: 1}, "24")
-			
+			this.animIn();
 		},
 		beforeDestroy: function() {
 	        //this.aboutOut();
 	    },
 		methods: {
+			animIn() {
+				var a = new TimelineMax();
+				a.to(this.m, 2.4, {scale: 1, ease: Elastic.easeOut.config(1, 0.5)}, "0")
+
+				a.to(this.l1, 2.4, {scale: 1, ease: Elastic.easeOut.config(1, 0.5)}, "0.6")
+				a.to(this.r1, 2.4, {scale: 1, ease: Elastic.easeOut.config(1, 0.5)}, "0.7")
+				a.to(this.l2, 2.4, {scale: 1, ease: Elastic.easeOut.config(1, 0.5)}, "0.8")
+				a.to(this.r2, 2.4, {scale: 1, ease: Elastic.easeOut.config(1, 0.5)}, "0.9")
+				a.to(this.l3, 2.4, {scale: 1, ease: Elastic.easeOut.config(1, 0.5)}, "1.0")
+				a.to(this.r3, 2.4, {scale: 1, ease: Elastic.easeOut.config(1, 0.5)}, "1.1")
+
+				// a.to(this.l1, 0, {transformOrigin: '100% 100%'}, "3.0")
+				// a.to(this.l2, 0, {transformOrigin: '100% 100%'}, "3.2")
+				// a.to(this.l3, 0, {transformOrigin: '100% 100%'}, "3.6")
+
+				a.to(this.b13, .6, {scale: 1, ease: Power3.easeOut}, "0")
+				a.to(this.b8, .6, {scale: 1, ease: Power3.easeOut}, "0.1")
+				a.to(this.b9, .6, {scale: 1, ease: Power3.easeOut}, "0.2")
+				a.to(this.b14, .6, {scale: 1, ease: Power3.easeOut}, "0.3")
+				a.to(this.b19, .6, {scale: 1, ease: Power3.easeOut}, "0.4")
+				a.to(this.b18, .6, {scale: 1, ease: Power3.easeOut}, "0.5")
+				a.to(this.b17, .6, {scale: 1, ease: Power3.easeOut}, "0.6")
+				a.to(this.b12, .6, {scale: 1, ease: Power3.easeOut}, "0.7")
+				a.to(this.b7, .6, {scale: 1, ease: Power3.easeOut}, "0.8")
+				a.to(this.b2, .6, {scale: 1, ease: Power3.easeOut}, "0.9")
+				a.to(this.b3, .6, {scale: 1, ease: Power3.easeOut}, "1.0")
+				a.to(this.b4, .6, {scale: 1, ease: Power3.easeOut}, "1.1")
+				a.to(this.b5, .6, {scale: 1, ease: Power3.easeOut}, "1.2")
+				a.to(this.b10, .6, {scale: 1, ease: Power3.easeOut}, "1.3")
+				a.to(this.b15, .6, {scale: 1, ease: Power3.easeOut}, "1.4")
+				a.to(this.b20, .6, {scale: 1, ease: Power3.easeOut}, "1.5")
+				a.to(this.b25, .6, {scale: 1, ease: Power3.easeOut}, "1.6")
+				a.to(this.b24, .6, {scale: 1, ease: Power3.easeOut}, "1.7")
+				a.to(this.b23, .6, {scale: 1, ease: Power3.easeOut}, "1.8")
+				a.to(this.b22, .6, {scale: 1, ease: Power3.easeOut}, "1.9")
+				a.to(this.b21, .6, {scale: 1, ease: Power3.easeOut}, "2.0")
+				a.to(this.b16, .6, {scale: 1, ease: Power3.easeOut}, "2.1")
+				a.to(this.b11, .6, {scale: 1, ease: Power3.easeOut}, "2.2")
+				a.to(this.b6, .6, {scale: 1, ease: Power3.easeOut}, "2.3")
+				a.to(this.b1, .6, {scale: 1, ease: Power3.easeOut}, "2.4")
+
+				TweenMax.to(this.l1, 4.8, {rotation: 7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 0});
+				TweenMax.to(this.r1, 4.8, {rotation: -7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 0.6});
+				TweenMax.to(this.l2, 4.8, {rotation: 7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 1.2});
+				TweenMax.to(this.r2, 4.8, {rotation: -7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 1.8});
+				TweenMax.to(this.l3, 4.8, {rotation: 7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 2.4});
+				TweenMax.to(this.r3, 4.8, {rotation: -7.5, ease: Power1.easeInOut, repeat: -1, yoyo: true, delay: 3.0});
+				
+				
+				
+
+				// a.to(t)
+
+				// var bg = new TimelineMax();
+				// var vh = 300 + 'vh';
+				// bg.to(this.p1, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "0")
+				// bg.to(this.p2, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "4.8")
+				// bg.to(this.p3, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "9.6")
+				// bg.to(this.p4, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "14.4")
+				// bg.to(this.p5, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "19.2")
+				// bg.to(this.p6, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "24")
+				// bg.to(this.p7, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "28.8")
+				// bg.to(this.p8, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "33.6")
+
+				// bg.to(this.p1, 19.2, {scale: 6, ease: Power4.easeOut}, "0")
+				// // bg.to(this.p1, 0, {scale: 1}, "9.6")
+				// bg.to(this.p2, 19.2, {scale: 6, ease: Power4.easeOut}, "2.4")
+				// // bg.to(this.p2, 0, {scale: 1}, "9.6")
+				// bg.to(this.p3, 19.2, {scale: 6, ease: Power4.easeOut}, "4.8")
+				// // bg.to(this.p3, 0, {scale: 1}, "12")
+				// bg.to(this.p4, 19.2, {scale: 6, ease: Power4.easeOut}, "7.2")
+				// // bg.to(this.p4, 0, {scale: 1}, "14.4")
+				// bg.to(this.p5, 19.2, {scale: 6, ease: Power4.easeOut}, "9.6")
+				// // bg.to(this.p5, 0, {scale: 1}, "16.8")
+				// bg.to(this.p6, 19.2, {scale: 6, ease: Power4.easeOut}, "12")
+				// // bg.to(this.p6, 0, {scale: 1}, "19.2")
+				// bg.to(this.p7, 19.2, {scale: 6, ease: Power4.easeOut}, "14.4")
+				// // bg.to(this.p7, 0, {scale: 1}, "21.6")
+				// bg.to(this.p8, 19.2, {scale: 6, ease: Power4.easeOut}, "16.8")
+				// // bg.to(this.p8, 0, {scale: 1}, "24")
+			},
 			toggle() {
 				var btn = document.getElementById(event.target.id);
 				// Toggle midi btn
@@ -368,6 +489,17 @@
 				if ( this.queue.length > 0 && this.playing === false ) {
 					console.log('play');
 					this.playing = setInterval(this.midi, 60000 / 200);
+					// BG anim
+					var bg = new TimelineMax();
+					var vh = 300 + 'vh';
+					bg.to(this.p1, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "0")
+					// bg.to(this.p2, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "4.8")
+					// bg.to(this.p3, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "9.6")
+					// bg.to(this.p4, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "14.4")
+					// bg.to(this.p5, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "19.2")
+					// bg.to(this.p6, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "24")
+					// bg.to(this.p7, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "28.8")
+					// bg.to(this.p8, 14.4, {width: vh, height: vh, ease: Power1.easeIn}, "33.6")
 				} else if ( this.tracks.length == 0 && this.queue.length == 0 ) {
 					console.log('stop');
 					clearInterval(this.playing);
@@ -400,7 +532,7 @@
 				}
 			},
 			midi() {
-				console.log(this.beat);
+				// console.log(this.beat);
 				if ( this.beat < 32 ) {
 					this.beat = this.beat + 1;
 
